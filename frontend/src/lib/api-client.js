ddde6ef429b06fcf7ab64bcefaa83c41fd857c9e
NABAJYOTI <117579785+Neyvil@users.noreply.github.com>
2025-01-07 17:00:07 +0530

    token issue resolveing

--- frontend/src/lib/api-client.js
+++ frontend/src/lib/api-client.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import { BACKEND_HOST } from "../../utils/constant.js";
 const apiClient = axios.create({
   baseURL: BACKEND_HOST,
+  withCredentials: true,
 });
 
 export { apiClient };
