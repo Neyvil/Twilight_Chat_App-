4ccc974659bf29eee680a36e3b3b75ef861e4945
NABAJYOTI <117579785+Neyvil@users.noreply.github.com>
2025-01-08 09:50:23 +0530

    checking issue

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,13 +4,13 @@ import "./index.css";
 import App from "./App.jsx";
 import { Toaster } from "./components/ui/sonner";
 import { SocketProvider } from "./context/SocketContext";
-import { BrowserRouter } from "react-router-dom";
+
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  
   <SocketProvider>
     <App />
     <Toaster position="top-right" closeButton />
   </SocketProvider>
-  </BrowserRouter>
+  
 );
