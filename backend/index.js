8e40cdaf03554e9b34d4401bb585b79c699247ef
NABAJYOTI <117579785+Neyvil@users.noreply.github.com>
2025-01-08 09:44:14 +0530

    revert

--- backend/index.js
+++ backend/index.js
@@ -40,7 +40,7 @@ app.use("/uploads/files", express.static("uploads/files"));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, '../frontend')));
 
 // API Routes
 app.use("/api/auth", authRoutes);
@@ -49,12 +49,9 @@ app.use("/api/messages", messagesRoutes);
 app.use("/api/channels", channelRoutes);
 
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend', 'index.html')); 
 });
-
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
